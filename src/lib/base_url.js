fa9508a2e9cd2f7d51ba6465cdff289da476b061
Himanshu-Deve <himanshu.swami@apnibus.com>
2025-11-02 22:14:18 +0530

    base url

--- src/lib/base_url.js
+++ src/lib/base_url.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000/' || 'http://localhost:8000/',
-  baseURL: 'https://desilentorder.in/api/',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000/' || 'http://localhost:8000/',
+  // baseURL: 'https://desilentorder.in/api/',
   headers: {
     'Content-Type': 'application/json',
   },
